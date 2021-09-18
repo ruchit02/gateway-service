77df24b4a7fe19cbb001c914f1d2e35b28951402
Ruchit Darji <darjiruchit02@gmail.com>
2021-09-18 20:37:10 +0530

    Initial launch

---
+++ src/main/java/com/topnotch/demo/PhotographyGatewayApplication.java
@@ -0,0 +1,90 @@
+package com.topnotch.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class PhotographyGatewayApplication {
+
+	@Autowired
+	private GatewayFilter authFilter ;
+	
+	@Value( "${com.topnotch.properties.service.protocol}" )
+	private String TRANSFER_PROTOCOL;
+	
+	@Value("${com.topnotch.properties.authservice.host}")
+	private String SERVICE_1_HOST ;
+	
+	@Value("${com.topnotch.properties.authservice.port}")
+	private String SERVICE_1_PORT ;
+	
+	@Value("${com.topnotch.properties.docuploadservice.host}")
+	private String SERVICE_2_HOST ;
+	
+	@Value("${com.topnotch.properties.docuploadservice.port}")
+	private String SERVICE_2_PORT ;
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PhotographyGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator gateway(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+				.route( preSpec -> preSpec.method( HttpMethod.GET )
+						  .and()
+						  .path( "/myapp/gateway/endpoint1" )
+				   .filters( filterSpec -> filterSpec.rewritePath( "/myapp/gateway/endpoint1" , "/myapp/authService/signupPage" ) )
+				   .uri( TRANSFER_PROTOCOL + "://" + SERVICE_1_HOST + ":" + SERVICE_1_PORT ) )
+				
+				.route( preSpec -> preSpec.method( HttpMethod.POST )
+						  .and()
+						  .path( "/myapp/gateway/endpoint1" )
+				   .filters( filterSpec -> filterSpec.rewritePath( "/myapp/gateway/endpoint1" , "/myapp/authService/signupPage" ) )
+				   .uri( TRANSFER_PROTOCOL + "://" + SERVICE_1_HOST + ":" + SERVICE_1_PORT ) )
+				
+				.route( preSpec -> preSpec.method( HttpMethod.GET )
+						  .and()
+						  .path( "/myapp/gateway/endpoint2" )
+				   .filters( filterSpec -> filterSpec.rewritePath( "/myapp/gateway/endpoint2" , "/myapp/authService/loginPage" ) )
+				   .uri( TRANSFER_PROTOCOL + "://" + SERVICE_1_HOST + ":" + SERVICE_1_PORT ) )
+				
+				.route( preSpec -> preSpec.method( HttpMethod.POST )
+						  .and()
+						  .path( "/myapp/gateway/endpoint2" )
+				   .filters( filterSpec -> filterSpec.rewritePath( "/myapp/gateway/endpoint2" , "/myapp/authService/authenticate" ) )
+				   .uri( TRANSFER_PROTOCOL + "://" + SERVICE_1_HOST + ":" + SERVICE_1_PORT ) )
+				
+				.route( preSpec -> preSpec.method( HttpMethod.GET )
+						  .and()
+						  .path( "/myapp/gateway/endpointToken" )
+				   .filters( filterSpec -> filterSpec.rewritePath( "/myapp/gateway/endpointToken" , "/myapp/authService/generateToken" ) )
+				   .uri( TRANSFER_PROTOCOL + "://" + SERVICE_1_HOST + ":" + SERVICE_1_PORT ) )
+				
+				.route( preSpec -> preSpec.method(HttpMethod.GET).and().path( "/myapp/gateway/endpoint3" )
+							       .filters( filterSpec -> {
+							    	   
+							    	   return filterSpec.filter(authFilter)
+							    	   		 .rewritePath( "/myapp/gateway/endpoint3" , "/myapp/serviceA/homePage" ) ;
+							       })
+							       .uri( TRANSFER_PROTOCOL + "://" + SERVICE_2_HOST + ":" + SERVICE_2_PORT ) )
+				
+				.route( preSpec -> preSpec.method(HttpMethod.POST).and().path( "/myapp/gateway/endpoint4" )
+								   .filters( filterSpec -> {
+									   return filterSpec.filter(authFilter)
+											  .rewritePath( "/myapp/gateway/endpoint4" , "/myapp/serviceA/uploadPage" ) ;
+								   }) 
+								   .uri( TRANSFER_PROTOCOL + "://" + SERVICE_2_HOST + ":" + SERVICE_2_PORT ) )
+				.build() ;
+	}
+}
